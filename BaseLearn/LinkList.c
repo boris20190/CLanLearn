3cd21c8732a3bfec81e5165e573141305171bafa
Boris <hao159784@gmail.com>
2021-08-20 16:01:35 +0800

    commit2

--- BaseLearn/LinkList.c
+++ BaseLearn/LinkList.c
@@ -7,20 +7,25 @@ typedef struct LinkNode {
 } *LinkList;
 
 LinkList create();
-LinkList detect(LinkList head,int address);
+
+LinkList detect(LinkList head, int address);
+
 void show(LinkList head);
-void delElement(LinkList head,int address);
-void addElement(LinkList head,int address,int data);
-void changeElement(LinkList head,int address,int data);
+
+void delElement(LinkList head, int address);
+
+void addElement(LinkList head, int address, int data);
+
+void changeElement(LinkList head, int address, int data);
 
 int main() {
-    LinkList head=create();
+    LinkList head = create();
     show(head);
-    addElement(head,4,1023);
+    addElement(head, 4, 1023);
     show(head);
-    delElement(head,5);
+    delElement(head, 5);
     show(head);
-    changeElement(head,15,99);
+    changeElement(head, 15, 99);
     show(head);
     return 0;
 }
@@ -30,9 +35,9 @@ LinkList create() {
     LinkList tmp = head;
     for (int i = 0; i < 10; ++i) {
         LinkList node = (LinkList) malloc(sizeof(LinkList));
-        node->data=i;
-        tmp->next=node;
-        tmp=tmp->next;
+        node->data = i;
+        tmp->next = node;
+        tmp = tmp->next;
     }
     return head;
 }
@@ -40,15 +45,15 @@ LinkList create() {
 void show(LinkList head) {
     LinkList tmp = head;
     while (tmp->next) {
-        tmp=tmp->next;
-        printf("%d ",tmp->data);
+        tmp = tmp->next;
+        printf("%d ", tmp->data);
     }
     printf("\n");
 }
 
-void delElement(LinkList head,int address) {
-    LinkList temp = detect(head,address);
-    if(temp==NULL) {
+void delElement(LinkList head, int address) {
+    LinkList temp = detect(head, address);
+    if (temp == NULL) {
         return;
     }
     LinkList del = temp->next;
@@ -56,9 +61,9 @@ void delElement(LinkList head,int address) {
     free(del);
 }
 
-void addElement(LinkList head,int address,int data) {
-    LinkList temp = detect(head,address);
-    if(temp==NULL) {
+void addElement(LinkList head, int address, int data) {
+    LinkList temp = detect(head, address);
+    if (temp == NULL) {
         return;
     }
     LinkList newNode = (LinkList) malloc(sizeof(LinkList));
@@ -67,15 +72,15 @@ void addElement(LinkList head,int address,int data) {
     temp->next = newNode;
 }
 
-void changeElement(LinkList head,int address,int data) {
-    LinkList temp = detect(head,address);
-    if(temp==NULL) {
+void changeElement(LinkList head, int address, int data) {
+    LinkList temp = detect(head, address);
+    if (temp == NULL) {
         return;
     }
     temp->data = data;
 }
 
-LinkList detect(LinkList head,int address) {
+LinkList detect(LinkList head, int address) {
     LinkList temp = head;
     for (int i = 0; i < address; ++i) {
         temp = temp->next;
