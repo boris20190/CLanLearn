eaf2f0330f4b1f58cc3fa5f168e4dd8d4de7a604
Boris <hao159784@gmail.com>
2021-10-30 21:43:38 +0800

    commit15

--- homework/10.20/demo01.c
+++ homework/10.20/demo01.c
@@ -58,13 +58,13 @@ void selection_sort(int arr[], int len) {
 void bubble_sort(int arr[], int len) {
     int i, j, temp;
     int fl = 1;
-    for (i = 1; fl;i++) {
+    for (i = 1; fl; i++) {
         fl = 0;
-        for (j = len-1; j >= i; --j) {
-            if(arr[j-1]>arr[j]) {
+        for (j = len - 1; j >= i; --j) {
+            if (arr[j - 1] > arr[j]) {
                 fl = 1;
-                temp = arr[j-1];
-                arr[j-1] = arr[j];
+                temp = arr[j - 1];
+                arr[j - 1] = arr[j];
                 arr[j] = temp;
             }
         }
